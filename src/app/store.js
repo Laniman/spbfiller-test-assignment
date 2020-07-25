4983492cb5d51208390cce6e576b1d92feb63840
George Achkasov <georgij-net@yandex.ru>
2020-07-25 13:41:25 +0300

    add elements generator

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,40 @@ import { configureStore } from '@reduxjs/toolkit';
 import { pageSlice } from '../features/page';
 import { elementsSlice } from '../features/elements';
 
+const random = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const generateColor = () => {
+  const r = random(0, 255).toString(16);
+  const g = random(0, 255).toString(16);
+  const b = random(0, 255).toString(16);
+
+  return `#${r}${g}${b}`;
+};
+
+const initialPageState = {
+  width: 400,
+  height: 400,
+};
+
+const generateElement = (id) => {
+  const minSize = 40;
+  const maxSize = 100;
+
+  const width = random(minSize, maxSize);
+  const height = random(minSize, maxSize);
+
+  return {
+    id,
+    width,
+    height,
+    backgroundColor: generateColor(),
+    x: random(0, initialPageState.width - width),
+    y: random(0, initialPageState.height - height),
+  };
+};
+
 const initialElementsState = [
   {
     id: 0,
@@ -19,16 +53,7 @@ const initialElementsState = [
     y: 200,
     backgroundColor: '#9E5FE4',
   },
-].concat(
-  Array.from({ length: 1000 }, (_, i) => ({
-    id: i + 2,
-    width: 40,
-    height: 100,
-    x: 100,
-    y: 200,
-    backgroundColor: '#9E5FE4',
-  })),
-);
+].concat(Array.from({ length: 1000 }, (_, i) => generateElement(i + 2)));
 
 export const store = configureStore({
   reducer: {
@@ -36,10 +61,7 @@ export const store = configureStore({
     elements: elementsSlice.reducer,
   },
   preloadedState: {
-    page: {
-      width: 400,
-      height: 400,
-    },
+    page: initialPageState,
     elements: initialElementsState,
   },
 });
